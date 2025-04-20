cd603c8df0a6318d7468c24d93918278a50cd300
Adi250799 <Sammuel454@gmail.com>
2025-04-20 07:42:12 +0200

    n

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
     return (
         <div>
-            <br><h2>Welcome to our website</h2></br>
+            <h2>Welcome to our website</h2>
             <p>
             The perfect digital notebook for organizing and keeping track of people's names and numbers. Simplify your life with our user-friendly platform designed for effective note-taking and seamless organization. Start managing your contacts efficiently today!
             </p>
